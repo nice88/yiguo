9ef942e84358c54caf73cd587e08aa640c7f9ca1
jing <1596566610@qq.com>
2019-06-22 17:14:45 +0800

    views

---
+++ src/views/header.vue
@@ -0,0 +1,41 @@
+<template>
+	<h2>
+	<slot name="jtou"></slot>
+	{{title}}
+	</h2>
+</template>
+
+<script>
+	export default{
+		name:"Header",
+		props:{
+			title:{
+				type:String,
+				default:"猫眼电影"
+			}
+		}
+	};
+</script>
+
+<style scoped>
+	h2{
+		height: 1.04rem;
+		background: #e54847;
+		color: #fff;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: .36rem;
+		font-weight: 400;
+		font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+		border-bottom: 1px solid #e54847;
+		position: relative;
+	}
+	img{
+		width:.5rem;
+		height: .5rem;
+		position: absolute;
+		left: .3rem;
+		top: .3rem;
+	}
+</style>
