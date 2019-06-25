fa6135b36eef0c26d8371aaf3b61c41f489d849b
kangzhongqi <271930112@qq.com>
2019-06-25 19:46:02 +0800

    “分类页面第二次提交 康中麒 6/25

--- src/views/Lclassify.vue
+++ src/views/Lclassify.vue
@@ -1,17 +1,19 @@
 <template>
     <div>
         <Search></Search>
-        <div>....</div>
+        <Classify></Classify>
     </div>
 </template>
 
 <script>
     // 声明
     import Search from"../components/classify/Search"
+    import Classify from'../components/classify/classification'
     export default {
         name: "Lclassify",
         components:{
             "Search":Search,
+            "Classify":Classify
         },
     }
 </script>
